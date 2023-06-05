58ab2a4388f00f5e2a642f7081736f4d77a805ab
berkatamas <berka.tamas@gmail.com>
2023-06-05 13:22:22 +0200

    update

--- src/test/java/hu/masterfield/pages/LoginPage.java
+++ src/test/java/hu/masterfield/pages/LoginPage.java
@@ -2,27 +2,28 @@ package hu.masterfield.pages;
 
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.devtools.v112.indexeddb.model.Key;
 
 import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byClassName;
-import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginPage extends BasePage {
 
-    SelenideElement acceptCookies = $(byClassName("beans-cookies-notification__button beans-button__container"));
-    //SelenideElement userNameInput = $(byId("user-name"));
-    //SelenideElement passwordInput = $(byId("password"));
-    //SelenideElement loginButton = $(byId("login-button"));
+    SelenideElement acceptCookies = $(byXpath("//*[@id=\"sticky-bar-cookie-wrapper\"]/span/div/div/div[2]/form[1]/button/span/span"));
 
     public void openPage() {
         open("https://bevasarlas.tesco.hu");
         isLoaded(acceptCookies);
     }
 
-    public void acceptCookies(){
+    public void acceptCookies() throws InterruptedException {
+        acceptCookies.hover();
+        Thread.sleep(1000);
         acceptCookies.click();
     }
 
