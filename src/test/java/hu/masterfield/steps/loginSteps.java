e7cc8136010d0509dbd1ced8c1b842bd2bd30fe1
berkatamas <berka.tamas@gmail.com>
2023-06-05 15:26:34 +0200

    update

--- src/test/java/hu/masterfield/steps/loginSteps.java
+++ src/test/java/hu/masterfield/steps/loginSteps.java
@@ -1,15 +1,28 @@
 package hu.masterfield.steps;
 
+import com.codeborne.selenide.Configuration;
 import hu.masterfield.pages.LoginPage;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class loginSteps {
     LoginPage loginPage = new LoginPage();
+
+    @Before
+    public static void setup() {
+        Configuration.reportsFolder = "target/reports";
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--incognito");
+        Configuration.browserCapabilities = options;
+    }
 
     @Given("open main page")
     public void openMainPage() {
